5df13d51964b14ee341d961be446fc0594600b4d
Isania27 <imranvuso@mail.ru>
2025-02-06 20:42:19 +0300

    Проверка что GitHub считает commit

---
+++ app/src/main/java/com/example/trialfragmentchanger/Fragment1.kt
@@ -0,0 +1,32 @@
+package com.example.trialfragmentchanger
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Fragment1 : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Fragment1()
+
+    }
+}
