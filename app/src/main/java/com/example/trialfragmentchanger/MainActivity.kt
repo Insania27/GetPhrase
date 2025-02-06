1db64eb87f93c7300f71ce073f5b159b37be168f
Isania27 <imranvuso@mail.ru>
2025-02-06 20:42:19 +0300

    Проверка что GitHub привязан

---
+++ app/src/main/java/com/example/trialfragmentchanger/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.trialfragmentchanger
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.trialfragmentchanger.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        replaceFragment(Fragment1())
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Fragment1())
+                R.id.add -> replaceFragment(Fragment2())
+                R.id.menu -> replaceFragment(Fragment3())
+                R.id.settings -> replaceFragment(Fragment4())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentLayout, fragment).commit()
+    }
+    var checkGitWorks = true
+}
